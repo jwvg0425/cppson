b2b4a7719995bd45c9ec59fc6345040bd0b0ba0e
namhyeonuk <jwvg0425@naver.com>
2015-10-20 19:48:40 +0900

    jsoncpp-compatible

--- src/cppson.hpp
+++ src/cppson.hpp
@@ -5,6 +5,7 @@
 #include <fstream>
 #include <functional>
 #include <vector>
+#include "json.h"
 #ifndef OUT
 #define OUT
 #endif
@@ -86,85 +87,15 @@ private:
 class JsonValue
 {
 public:
+
+	JsonValue() = default;
+	JsonValue(const Json::Value& value) : json(value) {}
 	
-	//if failed, return -1
-	int init(std::vector<std::string>& tokens, int offset)
+	//if failed, return false
+	bool init(const std::string& str)
 	{
-		//object
-		if (tokens[offset] == "{")
-		{
-			type = OBJECT;
-			offset++;
-			
-			while (offset < tokens.size())
-			{
-				if (tokens[offset] == "\"")
-				{
-					std::string& key = tokens[offset + 1];
-
-					if (tokens[offset + 2] != "\"" || tokens[offset + 3] != ":")
-						return -1;
-
-					offset = obj[key].init(tokens, offset + 4);
-
-					if (offset == -1)
-						return -1;
-				}
-				else if (tokens[offset] == ",")
-				{
-					offset++;
-				}
-				else if (tokens[offset] == "}")
-				{
-					return offset + 1;
-				}
-				else
-				{
-					return -1;
-				}
-			}
-			return -1;
-		}
-		else if (tokens[offset] == "[")
-		{
-			type = ARRAY;
-			offset++;
-			while (offset < tokens.size())
-			{
-				if (tokens[offset] == ",")
-				{
-					offset++;
-				}
-				else if (tokens[offset] == "]")
-				{
-					return offset + 1;
-				}
-				else
-				{
-					JsonValue value;
-					offset = value.init(tokens, offset);
-					if (offset == -1)
-						return -1;
-
-					arr.push_back(value);
-				}
-			}
-			return -1;
-		}
-		else
-		{
-			type = SINGLE;
-			if (tokens[offset] == "\"")
-			{
-				single.getStr() = tokens[offset + 1];
-				return offset + 3;
-			}
-			else
-			{
-				single.getStr() = tokens[offset];
-				return offset + 1;
-			}
-		}
+		Json::Reader reader;
+		return reader.parse(str.c_str(), json);
 	}
 
 	template<typename T>
@@ -172,12 +103,16 @@ public:
 	{
 		static_assert(std::is_base_of<Parsable<T>, T>::value, "T must be derived from Parsable<T>");
 
-		if (type != OBJECT)
-			return false;
-
-		for (auto m : obj)
+		if (!json.isObject())
 		{
-			if (!T::getMeta().at(m.first)(&val, m.second))
+			return false;
+		}
+
+		auto members = json.getMemberNames();
+
+		for (auto m : members)
+		{
+			if (!T::getMeta().at(m)(&val, json.get(m, Json::nullValue)))
 			{
 				return false;
 			}
@@ -195,14 +130,15 @@ public:
 					std::is_same<std::string, T>::value ||
 					std::is_same<bool, T>::value, "T must be derived from Parsable<T>, or T must be int or double or std::string or bool.");
 
-		if (type != ARRAY)
+		if (!json.isArray())
 			return false;
 
-		for (auto v : arr)
+		for (auto v : json)
 		{
 			T p;
+			JsonValue jsonVal(v);
 
-			if (v.parse(p))
+			if (jsonVal.parse(p))
 				val.push_back(p);
 			else
 				return false;
@@ -214,10 +150,10 @@ public:
 	template<>
 	bool parse<int>(int& val)
 	{
-		if (type != SINGLE)
+		if (!json.isInt())
 			return false;
 
-		val = single.getValue<int>();
+		val = json.asInt();
 
 		return true;
 	}
@@ -225,10 +161,10 @@ public:
 	template<>
 	bool parse<double>(double& val)
 	{
-		if (type != SINGLE)
+		if (!json.isDouble())
 			return false;
 
-		val = single.getValue<double>();
+		val = json.asDouble();
 
 		return true;
 	}
@@ -236,10 +172,10 @@ public:
 	template<>
 	bool parse<float>(float& val)
 	{
-		if (type != SINGLE)
+		if (!json.isDouble())
 			return false;
 
-		val = single.getValue<double>();
+		val = json.asFloat();
 
 		return true;
 	}
@@ -247,128 +183,49 @@ public:
 	template<>
 	bool parse<bool>(bool& val)
 	{
-		if (type != SINGLE)
-			return false;
-
-		val = single.getValue<bool>();
-
-		return true;
-	}
-
-	template<>
-	bool parse<std::string>(std::string& val)
-	{
-		if (type != SINGLE)
-			return false;
-
-		val = single.getValue<std::string>();
-
-		return true;
-	}
-
-private:
-	enum Type
-	{
-		NONE,
-		SINGLE,
-		OBJECT,
-		ARRAY
-	};
-
-	Type type;
-	Single single;
-	std::map<std::string, JsonValue> obj;
-	std::vector<JsonValue> arr;
-};
-
-bool tokenize(const std::string& str, OUT std::vector<std::string>& tokens)
-{
-	std::string token;
-	std::string space = " \t\r\n";
-	std::string delim = ":{}[],";
-	std::string number = "0123456789.";
-	int state = 0;
-
-	for (size_t i = 0; i < str.size(); i++)
-	{
-		switch (state)
+		if (json.isBool())
 		{
-		case 0:
-			//ignore space
-			if (space.find(str[i]) != std::string::npos)
+			val = json.asBool();
+			return true;
+		}
+		else if (json.isString())
+		{
+			std::string str = json.asString();
+			if (str == "true")
 			{
-				if (token.size() != 0)
-				{
-					tokens.push_back(token);
-					token.clear();
-				}
-
-				continue;
+				val = true;
+				return true;
 			}
-			//go string
-			else if (str[i] == '"')
+			else if (str == "false")
 			{
-				if (token.size() != 0)
-					return false;
-
-				tokens.push_back({ str[i] });
-				state = 1;
-			}
-			//one char token
-			else if (delim.find(str[i]) != std::string::npos)
-			{
-				if (token.size() != 0)
-				{
-					tokens.push_back(token);
-					token.clear();
-				}
-				tokens.push_back({ str[i] });
-			}
-			//number
-			else if (number.find(str[i]) != std::string::npos)
-			{
-				token.push_back({ str[i] });
-			}
-			//true or false
-			else if (str.substr(i, 4) == "true")
-			{
-				if (token.size() != 0)
-					return false;
-
-				tokens.push_back(str.substr(i, 4));
-				i += 3;
-			}
-			else if (str.substr(i, 5) == "false")
-			{
-				if (token.size() != 0)
-					return false;
-
-				tokens.push_back(str.substr(i, 5));
-				i += 4;
+				val = false;
+				return true;
 			}
 			else
 			{
 				return false;
 			}
-			break;
-		case 1:
-			if (str[i] == '"')
-			{
-				tokens.push_back(token);
-				tokens.push_back({ str[i] });
-				token.clear();
-				state = 0;
-			}
-			else
-			{
-				token.push_back({ str[i] });
-			}
-			break;
+		}
+		else
+		{
+			return false;
 		}
 	}
 
-	return true;
-}
+	template<>
+	bool parse<std::string>(std::string& val)
+	{
+		if (!json.isString())
+			return false;
+
+		val = json.asString();
+
+		return true;
+	}
+
+private:
+	Json::Value json;
+};
 
 template<typename T>
 class Parsable
@@ -411,16 +268,9 @@ bool loadFile(T& value, const std::string& fileName)
 template<typename T>
 bool loadString(T& value, const std::string& str)
 {
-	std::vector<std::string> tokens;
-
-	if (!tokenize(str, tokens))
-	{
-		return false;
-	}
-
 	JsonValue json;
 
-	if (json.init(tokens, 0) == -1)
+	if (!json.init(str))
 		return false;
 
 	json.parse(value);
